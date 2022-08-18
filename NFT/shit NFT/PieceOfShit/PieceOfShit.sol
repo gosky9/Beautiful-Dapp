ef2bc47cc5912c811fa6b37976e1a2ac0727561b
gosky9 <1079330450@qq.com>
2022-08-18 19:53:44 +0800

    创建shit

---
+++ NFT/shit NFT/PieceOfShit/PieceOfShit.sol
@@ -0,0 +1,200 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "erc721a/contracts/extensions/ERC721AQueryable.sol";
+
+contract PieceOfShit is ERC2981, ERC721AQueryable, Ownable {
+    using Address for address payable;
+    using Strings for uint256;
+
+    event LuckyShit(address minter, uint32 amount);
+
+    bytes32 public immutable _lotterySalt; // 彩票的盐值？？
+    uint256 public immutable _price;
+    uint32 public immutable _maxSupply;
+    uint32 public immutable _teamSupply;
+    uint32 public immutable _instantFreeSupply; // 立即免费的数量？？
+    uint32 public immutable _randomFreeSupply;  // 随机免费的数量？？
+    uint32 public immutable _instantFreeWalletLimit; 
+    uint32 public immutable _walletLimit;
+
+    uint32 public _teamMinted;
+    uint32 public _randomFreeMinted;
+    uint32 public _instantFreeMinted;
+    bool public _started;
+    string public _metadataURI = "https://metadata.pieceofshit.wtf/json/";
+
+    struct Status {
+        // config
+        uint256 price;
+        uint32 maxSupply;
+        uint32 publicSupply;
+        uint32 instantFreeSupply;
+        uint32 randomFreeSupply;
+        uint32 instantFreeWalletLimit;
+        uint32 walletLimit;
+
+        // state
+        uint32 publicMinted;
+        uint32 instantFreeMintLeft;
+        uint32 randomFreeMintLeft;
+        uint32 userMinted;
+        bool soldout;
+        bool started;
+    }
+
+    constructor(
+        uint256 price,
+        uint32 maxSupply,
+        uint32 teamSupply,
+        uint32 instantFreeSupply,
+        uint32 randomFreeSupply,
+        uint32 instantFreeWalletLimit,
+        uint32 walletLimit
+    ) ERC721A("pieceofshit", "SHIT") { // 合约名称，合约简称，需要修改的地方
+        require(maxSupply >= teamSupply + instantFreeSupply);
+        require(maxSupply - teamSupply - instantFreeSupply >= randomFreeSupply);
+
+        _lotterySalt = keccak256(abi.encodePacked(address(this), block.timestamp)); // ??? 1
+        _price = price;
+        _maxSupply = maxSupply;
+        _teamSupply = teamSupply;
+        _instantFreeSupply = instantFreeSupply;
+        _instantFreeWalletLimit = instantFreeWalletLimit;
+        _randomFreeSupply = randomFreeSupply;
+        _walletLimit = walletLimit;
+
+        setFeeNumerator(750); // ??? 2
+    }
+
+    function mint(uint32 amount) external payable {
+        require(_started, "PieceOfShit: Sale is not started");
+
+        uint32 publicMinted = _publicMinted(); // 公开minted = 已经mint-团队mint
+        uint32 publicSupply = _publicSupply(); // 公开supply = 总supply-团队supply 
+        require(amount + publicMinted <= _publicSupply(), "PieceOfShit: Exceed max supply");
+
+        uint32 minted = uint32(_numberMinted(msg.sender));
+        require(amount + minted <= _walletLimit, "PieceOfShit: Exceed wallet limit"); // 每个钱包最多mint限制
+
+        uint32 instantFreeWalletLimit = _instantFreeWalletLimit;
+        uint32 freeAmount = 0;
+        if (minted < instantFreeWalletLimit) {
+            uint32 quota = instantFreeWalletLimit - minted;
+            freeAmount += quota > amount ? amount : quota;
+        }
+ 
+        if (minted + amount > instantFreeWalletLimit) {
+            uint32 enterLotteryAmount = amount - instantFreeWalletLimit;
+            uint32 randomFreeAmount = 0;
+            uint32 randomFreeMinted = _randomFreeMinted;
+            uint32 quota = _randomFreeSupply - randomFreeMinted;
+            // 随机免费，会把钱返还
+            if (quota > 0) {
+                uint256 randomSeed = uint256(keccak256(abi.encodePacked(
+                    msg.sender,
+                    publicMinted,
+                    block.difficulty,
+                    _lotterySalt)));
+
+                for (uint256 i = 0; i < enterLotteryAmount && quota > 0; ) {
+                    if (uint16((randomSeed & 0xFFFF) % publicSupply) < quota) {
+                        randomFreeAmount += 1;
+                        quota -= 1;
+                    }
+
+                    unchecked {
+                        i++;
+                        randomSeed = randomSeed >> 16;
+                    }
+                }
+
+                if (randomFreeAmount > 0) {
+                    freeAmount += randomFreeAmount;
+                    _randomFreeMinted += randomFreeAmount;
+                    emit LuckyShit(msg.sender, randomFreeAmount);
+                }
+            }
+        }
+        // 检查钱够不够
+        uint256 requiredValue = (amount - freeAmount) * _price;
+        require(msg.value >= requiredValue, "PieceOfShit: Insufficient fund");
+
+        _safeMint(msg.sender, amount);
+        if (msg.value > requiredValue) { // 把钱返还
+            payable(msg.sender).sendValue(msg.value - requiredValue);
+        }
+    }
+
+    function _publicMinted() public view returns (uint32) { // 总公开minted
+        return uint32(_totalMinted()) - _teamMinted;
+    }
+
+    function _publicSupply() public view returns (uint32) {  // 总公开supply
+        return _maxSupply - _teamSupply;
+    }
+
+    function _status(address minter) external view returns (Status memory) {  // 查看状态
+        uint32 publicSupply = _maxSupply - _teamSupply;
+        uint32 publicMinted = uint32(ERC721A._totalMinted()) - _teamMinted;
+
+        return Status({
+            // config
+            price: _price,
+            maxSupply: _maxSupply,
+            publicSupply:publicSupply,
+            instantFreeSupply: _instantFreeSupply,
+            randomFreeSupply: _randomFreeSupply,
+            instantFreeWalletLimit: _instantFreeWalletLimit,
+            walletLimit: _walletLimit,
+
+            // state
+            publicMinted: publicMinted,
+            instantFreeMintLeft: _instantFreeSupply - _instantFreeMinted,
+            randomFreeMintLeft: _randomFreeSupply - _randomFreeMinted,
+            soldout:  publicMinted >= publicSupply,
+            userMinted: uint32(_numberMinted(minter)),
+            started: _started
+        });
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) { // 查看单个tokenURI，给opensea看的
+        if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+
+        string memory baseURI = _metadataURI;
+        return string(abi.encodePacked(baseURI, tokenId.toString(), ".json"));
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC2981, ERC721A) returns (bool) { // 支持接口，应该是屎怪调用的
+        return
+            interfaceId == type(IERC2981).interfaceId ||
+            interfaceId == type(IERC721).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+
+    function devMint(address to, uint32 amount) external onlyOwner { // 团队mint
+        _teamMinted += amount;
+        require(_teamMinted <= _teamSupply, "PieceOfShit: Exceed max supply");
+        _safeMint(to, amount);
+    }
+
+    function setFeeNumerator(uint96 feeNumerator) public onlyOwner {  // ？？？ 3
+        _setDefaultRoyalty(owner(), feeNumerator);
+    }
+
+    function setStarted(bool started) external onlyOwner { // 公售开关
+        _started = started;
+    }
+
+    function setMetadataURI(string memory uri) external onlyOwner { // 设置metadataURI
+        _metadataURI = uri;
+    }
+
+    function withdraw() external onlyOwner { // 取钱
+        payable(msg.sender).sendValue(address(this).balance);
+    }
+}
---
+++ NFT/shit NFT/readme.md
@@ -0,0 +1,72 @@
+# 项目简介
+
+shit 原名 I'll poop it，是在2022年6月1日开始很火的一套讽刺类nft和token，
+
+I'll poop it NFT，这一阶段只是单纯的图片NFT项目，以一种嘲讽的态度来表达对市场的炒作的不满，正值熊市，各种NFT乱飞，如BAYC、哥布林等，屎在这时出现了一定的热度，一开始freemint，后来涨到几百u左右。
+
+它包括6种nft和1个ERC20 token。
+
+这里讲解发展史和技术细节
+
+# 玩法
+
+这个项目包括多个阶段，看起来是项目方后来看到有热度再继续推出的，包含了当前nft市场的几乎所有玩法，
+
+0. [twitter](https://twitter.com/pieceofshit_wtf)
+1. [OG poop](https://opensea.io/collection/pieceofshit-wtf)
+2. [像素poop](https://opensea.io/collection/pixelverse-poops/activity)
+3. [ShitPlunger:马桶刷](https://opensea.io/collection/shitplunger/activity)
+4. [ShitBeast:屎怪.分创世和普通](https://opensea.io/collection/shitbeast/activity)
+5. [NoShitZone：清洁剂（金、绿、粉）](https://opensea.io/collection/noshitzone/activity)
+6. [De-Generations: S 第三代NFT](https://opensea.io/collection/de-generations-s)
+
+## 第一阶段（5月30日-6月4日） 
+
+从技术上来说，这一阶段的NFT合约 ，mint的时候能抽奖，抽到奖为free.其中777个为随机的Free mint，剩余则需要花费 0.0077 ETH，总量为10,000
+TODO..
+
+## 第二阶段（6月5日-6月8日）
+6月5日凌晨12点40左右，官方推特发出合成公告，
+
+燃烧掉两个屎可以合成一个【屎兽】+【100亿个ShitCoin】，共可以burn掉7810个 OG 屎。
+
+其中有 7810 个 OG 屎合成了 3905 个创世屎怪，早期合成得来的3905个屎兽具有【genesis】属性，后期会直接空投一个【清洁剂】
+
+另外有 6095 个普通屎怪以 0.06 ETH 价格公售 。
+
+共 3 905 + 6 095 = 10 000个
+
+技术上，这一阶段有 
+
+1. burn掉 OG 屎， mint 创世屎怪, transfer 100e shit代币
+2. 0.06e mint 普通屎怪
+
+## 第三阶段，6月9日——官方公告（有预热说明马桶刷玩法）
+
+每个创世屎怪获得空投 2个清洁剂(马桶搋)，每个 OG POOP 持有者会获得 2 个 ShitPlungers （马桶刷）的空投，合计空投了 4416 个。
+
+可以用 $SHIT mint马桶搋，共4472个，马桶搋的mint采用荷兰拍和反荷兰拍的方式 0.05e
+
+屎+shitPlunger = 【像素风NFT】，屎+马桶塞消失burn
+
+总供给为 8888 个
+
+利好1.OG NFT赋能 2. $SHIT通缩
+
+## 第四阶段，屎兽NFT进化
+
+shitbeast + 3*洁厕灵(NoShitZone) = 最终屎人De-Generations:S
+屎怪+清洁剂→屎人
+洁厕灵消失，shitbeast不清楚
+
+OG的shitbeast NFT是有空投2个清洁剂NFT的，空投的是金色和绿色的版本,金色的效果很好。
+非OG的屎兽  需要自己花0.65eth，mint清洁剂nft,mint的是粉色版本目前清洁剂的的价格在0.3ETH左右
+
+## 第？阶段 2022.8.18 出了游戏
+
+# 参考链接
+
+一个屎玩法的介绍
+https://mirror.xyz/lindalinda.eth/9_ymIAKrHnl65QZr6H-RhqVyOrAAUCP1R3BwqRlLqOw
+
+https://mirror.xyz/styleschen.eth/xoj3pmtj1OyU3SNK2qDgSIVjz8knIH7KaIljS-usFRs
